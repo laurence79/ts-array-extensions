a76c665d9f849871f50a6e930e32c6639582a8aa
Laurence Hartgill <laurence@hartgill.co.uk>
2021-01-31 16:48:14 +0000

    Add last function (#14)

---
+++ src/last/last.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-restricted-syntax */
+export {};
+
+declare global {
+  interface Array<T> {
+    /**
+     * Returns the last element of an array or `undefined` if it is empty.
+     */
+    last(): T | undefined;
+
+    /**
+     * Calls a defined callback function on each element of an array until
+     * one returns `true` or there are no more elements.
+     *
+     * @param callbackFn - A function that accepts up to three arguments. The
+     *  last method calls the callbackfn function up to one time for each
+     *  element in the array.
+     *
+     * @returns The last element where the callbackFn returned true, or if none
+     *  do then `undefined`
+     */
+    last(
+      callbackFn: (element: T, index: number, array: T[]) => boolean
+    ): T | undefined;
+  }
+}
+
+if (!Array.prototype.last) {
+  // eslint-disable-next-line no-extend-native
+  Array.prototype.last = function last<T>(
+    this: T[],
+    callbackFn?: (element: T, index: number, array: T[]) => boolean
+  ): T | undefined {
+    if (!callbackFn) {
+      if (this.length > 0) {
+        return this[this.length - 1];
+      }
+      return undefined;
+    }
+
+    return this.reduce((memo, element, index) => {
+      if (callbackFn(element, index, this)) {
+        memo = element;
+      }
+      return memo;
+    }, undefined as undefined | T);
+  };
+}
